1408db2572e684f40f33dd3134d94023f07d89aa
cd <12>
2020-03-18 16:08:29 +0000

    devise app

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,7 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title)
+      params.require(:event).permit(:title, :brief, :event_start, :event_end, :location)
     end
+
 end
