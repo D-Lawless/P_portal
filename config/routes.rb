e4d1bdb5ec4fd7d19921f2237d0159706dc58aaf
cd <12>
2020-03-19 15:57:21 +0000

    Profiles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  resources :users, :only =>[:show]
   resources :events
   root "events#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
